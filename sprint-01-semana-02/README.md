bdddf7fdbe4fddbe35d1164fde94fa31c0bafcc9
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-07-31 13:58:23 -0400

    docs: Adicionado README.md da semana-02

---
+++ sprint-01-semana-02/README.md
@@ -0,0 +1,1 @@
+### Erik Miqueias adiciona Wuesley Nogueira
---
+++ sprint-01-semana-02/projeto-01/scripts.js
@@ -0,0 +1,32 @@
+const display = document.querySelector("#result");
+const weight = document.querySelector("#weight");
+const height = document.querySelector("#height");
+const buttonImc = document.querySelector("#result-button");
+const situation = document.querySelector("#situation");
+
+function imcCalc(weight, height) {
+  return Number(weight / (height * height));
+}
+
+buttonImc.addEventListener("click", () => {
+  if (!weight.value || !height.value) return;
+
+  const imc = imcCalc(weight.value, height.value);
+
+  display.innerHTML = imc.toFixed(1);
+
+  if (imc < 18.5) {
+    situation.innerHTML = "Magreza";
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    situation.innerHTML = "Normal";
+  } else if (imc >= 25 && imc <= 29.9) {
+    situation.innerHTML = "Obesidade Grau I";
+  } else if (imc >= 30 && imc <= 39.9) {
+    situation.innerHTML = "Obesidade Grau II";
+  } else {
+    situation.innerHTML = "Obesidade Grau III";
+  }
+
+  weight.value = "";
+  height.value = "";
+});
